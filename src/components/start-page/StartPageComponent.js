2e7c2b1111a19c76226364b944c2697a96195a96
yehorchernenko <lordo916@gmail.com>
2019-05-01 10:55:46 +0300

    Added app bar to start page

---
+++ src/components/start-page/StartPageComponent.js
@@ -0,0 +1,58 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import {withStyles} from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu'
+
+
+const styles = {
+    root: {
+        flexGrow: 1,
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20
+    },
+};
+
+class StartPageComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {auth: false}
+    }
+
+    handleAuth = () => {
+      this.setState({auth: !this.state.auth})
+    };
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" className={classes.grow}>
+                            Delivery+
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
+    }
+}
+
+StartPageComponent.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+export default withStyles(styles)(StartPageComponent)
